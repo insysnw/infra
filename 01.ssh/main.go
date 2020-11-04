6d26a818116d837f43b4a3622eb55bc0fce61c4d
Vlad Petrov <ejiek@mail.ru>
2020-11-05 02:27:03 +0300

    move sshkey stuff out of the main

--- 01.ssh/main.go
+++ 01.ssh/main.go
@@ -11,6 +11,74 @@ import (
 	"strings"
 )
 
+type SshKey struct {
+	KeyType   string
+	KeyItself string
+	Comment   string
+}
+
+func (key SshKey) toString() string {
+	return key.KeyType + " " + key.KeyItself + " " + key.Comment
+}
+
+func (key SshKey) toDO(ctx *pulumi.Context) (*digitalocean.SshKey, error) {
+	name := "insys-key: " + key.Comment
+	newKey, err := digitalocean.NewSshKey(ctx, name, &digitalocean.SshKeyArgs{
+		Name:      pulumi.String(name),
+		PublicKey: pulumi.String(key.toString()),
+	})
+	if err != nil {
+		return newKey, err
+	}
+	return newKey, nil
+}
+
+func GetKeys(ctx *pulumi.Context, keysFilePath string) ([]*digitalocean.SshKey, error) {
+	var Keys []*digitalocean.SshKey
+
+	localKeys, err := ReadKeys(keysFilePath)
+	if err != nil {
+		return Keys, err
+	}
+
+	for _, localKey := range localKeys {
+		newKey, err := localKey.toDO(ctx)
+		if err != nil {
+			return Keys, err
+		} else {
+			Keys = append(Keys, newKey)
+		}
+	}
+	return Keys, nil
+}
+
+func ReadKeys(keysFilePath string) ([]SshKey, error) {
+	f, err := os.Open(keysFilePath)
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+
+	var Keys []SshKey
+
+	for scanner.Scan() {
+		pubKey := scanner.Text()
+		parsedPubKey := strings.Split(pubKey, " ")
+		// TODO: add some form of validation
+		parsedComment := strings.Join(parsedPubKey[2:], " ")
+		Keys = append(Keys, SshKey{
+			KeyType:   parsedPubKey[0],
+			KeyItself: parsedPubKey[1],
+			Comment:   parsedComment,
+		})
+	}
+	return Keys, nil
+
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
@@ -27,66 +95,17 @@ func main() {
 		installationScript := string(content)
 
 		// read students keys file
-		fStudents, err := os.Open(studentsKeysFilePath)
-
+		StudentsKeys, err := GetKeys(ctx, studentsKeysFilePath)
 		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-
-		defer fStudents.Close()
-
-		scanner := bufio.NewScanner(fStudents)
-
-		// create each key
-		var StudentsKeys []*digitalocean.SshKey
-		for scanner.Scan() {
-			pubKey := scanner.Text()
-			parsedPubKey := strings.Split(pubKey, " ")
-			keyComment := parsedPubKey[2]
-			name := "insys-key: " + keyComment
-			newKey, err := digitalocean.NewSshKey(ctx, name, &digitalocean.SshKeyArgs{
-				Name:      pulumi.String(name),
-				PublicKey: pulumi.String(pubKey),
-			})
-			if err != nil {
-				fmt.Println("Unable to add key for " + keyComment)
-				fmt.Println(err)
-			} else {
-				StudentsKeys = append(StudentsKeys, newKey)
-			}
+			fmt.Println("Unable to read keys from " + studentsKeysFilePath)
+			fmt.Println(err)
 		}
 
 		// read teachers keys file
-		fTeachers, err := os.Open(teachersKeysFilePath)
-
+		TeachersKeys, err := GetKeys(ctx, teachersKeysFilePath)
 		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-
-		defer fTeachers.Close()
-
-		scanner = bufio.NewScanner(fTeachers)
-
-		if err := scanner.Err(); err != nil {
-			fmt.Println("Error: ", err)
-		}
-
-		var TeachersKeys []*digitalocean.SshKey
-		for scanner.Scan() {
-			pubKey := scanner.Text()
-			parsedPubKey := strings.Split(pubKey, " ")
-			keyComment := parsedPubKey[2]
-			name := "insys-key: " + keyComment
-			newKey, err := digitalocean.NewSshKey(ctx, name, &digitalocean.SshKeyArgs{
-				Name:      pulumi.String(name),
-				PublicKey: pulumi.String(pubKey),
-			})
-			if err != nil {
-				fmt.Println("Unable to add key for " + keyComment)
-				fmt.Println(err)
-			} else {
-				TeachersKeys = append(TeachersKeys, newKey)
-			}
+			fmt.Println("Unable to read keys from " + teachersKeysFilePath)
+			fmt.Println(err)
 		}
 
 		insysVpc, err := digitalocean.NewVpc(ctx, "insysVpc", &digitalocean.VpcArgs{
